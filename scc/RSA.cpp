6bd25cce69e7b5873c7240f94e1f77381c21427e
RIscRIpt <richard@riscript.com>
2018-08-16 16:51:57 +0300

    RSA: add constructor for random key, add getters of components

--- scc/RSA.cpp
+++ scc/RSA.cpp
@@ -5,15 +5,32 @@
 #include <openssl/bn.h>
 #include <openssl/err.h>
 
+scc::RSA::RSA(unsigned bits, scb::Bytes const &publicExponent) {
+    rsa_ = RSA_new();
+    auto BNpublicExponent = BN_bin2bn(publicExponent.data(), static_cast<int>(publicExponent.size()), NULL);
+
+    int result = RSA_generate_key_ex(
+        static_cast<::RSA*>(rsa_),
+        bits,
+        BNpublicExponent,
+        NULL
+    );
+
+    if (!result) {
+        free();
+        throw Exception(result);
+    }
+}
+
 scc::RSA::RSA(scb::Bytes const &modulus, scb::Bytes const &exponent) {
     rsa_ = RSA_new();
-    modulus_ = BN_bin2bn(modulus.data(), static_cast<int>(modulus.size()), NULL);
-    exponent_ = BN_bin2bn(exponent.data(), static_cast<int>(exponent.size()), NULL);
+    auto BNmodulus = BN_bin2bn(modulus.data(), static_cast<int>(modulus.size()), NULL);
+    auto BNexponent = BN_bin2bn(exponent.data(), static_cast<int>(exponent.size()), NULL);
 
     int result = RSA_set0_key(
         static_cast<::RSA*>(rsa_),
-        static_cast<BIGNUM*>(modulus_),
-        static_cast<BIGNUM*>(exponent_),
+        static_cast<BIGNUM*>(BNmodulus),
+        static_cast<BIGNUM*>(BNexponent),
         NULL
     );
 
@@ -28,7 +45,11 @@ scc::RSA::~RSA() {
 }
 
 scb::Bytes scc::RSA::transorm(scb::Bytes const &bytes) const {
-    scb::Bytes result(BN_num_bits(static_cast<BIGNUM*>(modulus_)) / 8);
+    BIGNUM const *modulus = NULL;
+    RSA_get0_key(static_cast<::RSA*>(rsa_), &modulus, NULL, NULL);
+    if (!modulus)
+        throw std::runtime_error("failed to get modulus");
+    scb::Bytes result(BN_num_bytes(modulus));
     if (
         RSA_public_decrypt(
             static_cast<int>(bytes.size()),
@@ -40,6 +61,36 @@ scb::Bytes scc::RSA::transorm(scb::Bytes const &bytes) const {
     ) {
         throw Exception(ERR_get_error());
     }
+    return result;
+}
+
+scb::Bytes scc::RSA::get_modulus() const {
+    BIGNUM const *modulus = NULL;
+    RSA_get0_key(static_cast<::RSA*>(rsa_), &modulus, NULL, NULL);
+    if (!modulus)
+        return {};
+    scb::Bytes result(BN_num_bytes(modulus));
+    BN_bn2bin(modulus, result.data());
+    return result;
+}
+
+scb::Bytes scc::RSA::get_public_exponent() const {
+    BIGNUM const *exponent = NULL;
+    RSA_get0_key(static_cast<::RSA*>(rsa_), NULL, &exponent, NULL);
+    if (!exponent)
+        return {};
+    scb::Bytes result(BN_num_bytes(exponent));
+    BN_bn2bin(exponent, result.data());
+    return result;
+}
+
+scb::Bytes scc::RSA::get_private_exponent() const {
+    BIGNUM const *exponent = NULL;
+    RSA_get0_key(static_cast<::RSA*>(rsa_), NULL, NULL, &exponent);
+    if (!exponent)
+        return {};
+    scb::Bytes result(BN_num_bytes(exponent));
+    BN_bn2bin(exponent, result.data());
     return result;
 }
 
--- scc/RSA.h
+++ scc/RSA.h
@@ -16,17 +16,26 @@ namespace scc {
         };
         */
 
+        RSA(unsigned bits, scb::Bytes const &publicExponent);
         RSA(scb::Bytes const &modulus, scb::Bytes const &exponent);
         ~RSA();
 
+        RSA(RSA &&other) = default;
+        RSA& operator=(RSA &&rhs) = default;
+
+        RSA(RSA const &other) = delete;
+        RSA& operator=(RSA const &rhs) = delete;
+
         scb::Bytes transorm(scb::Bytes const &bytes) const;
+
+        scb::Bytes get_modulus() const;
+        scb::Bytes get_public_exponent() const;
+        scb::Bytes get_private_exponent() const;
 
     private:
         void free();
 
         void *rsa_;
-        void *modulus_;
-        void *exponent_;
     };
 
 }
