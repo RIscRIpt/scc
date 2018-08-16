b2cad249ff6106a4833eeb533f63fe71df53ce98
RIscRIpt <richard@riscript.com>
2018-08-16 17:12:50 +0300

    Remove useless comment

--- scc/RSA.h
+++ scc/RSA.h
@@ -6,16 +6,6 @@ namespace scc {
 
     class RSA final {
     public:
-        /*
-        enum class Padding {
-            PKCS1 = RSA_PKCS1_PADDING,
-            SSLV23 = RSA_SSLV23_PADDING,
-            No = RSA_NO_PADDING,
-            PKCS1_OAEP = RSA_PKCS1_OAEP_PADDING,
-            X931 = RSA_X931_PADDING,
-        };
-        */
-
         RSA(unsigned bits, scb::Bytes const &publicExponent);
         RSA(scb::Bytes const &modulus, scb::Bytes const &exponent);
         ~RSA();
